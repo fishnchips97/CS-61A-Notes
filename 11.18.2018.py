b32a658f7f50193350a79051016a9bc93134b335
Erik Fisher <erikfisher@Eriks-MacBook-Air.local>
2018-06-19 10:04:21 -0700

    6.18.2018
    First day of lecture notes. 6/18/2018

---
+++ 11.18.2018.py
@@ -0,0 +1,14 @@
+# Erik Fisher
+# 6/18/2018 - Day 1
+# CS 61A Notes
+
+# Introduction to Python
+# Expressions: 20 + 17 or sin(pi)
+# Operators: +, -, /, *
+
+# Call Expressions: add(2, 3)
+#              operator(operand, operand)
+from operator import add, mul
+
+print(add(add(6, mul(4,6)), mul(3,5)))
+# prints 45
